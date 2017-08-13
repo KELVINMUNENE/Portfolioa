dfea03129017b27bbee4e83a590b3d3f2d6f46f4
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-13 19:40:25 +0300

    created link

--- db/migrate/20170813163820_create_join.rb
+++ db/migrate/20170813163820_create_join.rb
@@ -1,6 +1,11 @@
 class CreateJoin < ActiveRecord::Migration[5.1]
   def change
     create_table :joins do |t|
+    	t.column :project_id, :integer
+    	t.column :skill_id, :integer
     end
   end
 end
+
+
+
