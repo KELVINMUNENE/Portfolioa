53f9d8c3b9377781a0bb095b57fe9b664d216a1d
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-13 18:02:16 +0300

    created a migration

--- db/migrate/20170813145829_create_skills.rb
+++ db/migrate/20170813145829_create_skills.rb
@@ -2,7 +2,7 @@ class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
     t.column :name, :string
-    t.column :ability, :integer
+    t.column :rating, :integer
   
 
      t.timestamps
