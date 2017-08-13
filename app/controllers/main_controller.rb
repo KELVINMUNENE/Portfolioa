ed69863220a3eb440f93481f9d6a56ddcb24f14b
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-13 19:02:23 +0300

    created a projects folder

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,7 @@
 class MainController < ApplicationController
   def index
     @skills = Skill.all
-    
+    @projects = Project.all
   end
 end
+
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,44 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+    @skills = Skill.all
+  end
+  def show
+    @skills = Skill.all
+    @project = Project.find(params[:id])
+
+  end
+  def new
+    @project = Project.new
+  end
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_path
+    else
+      render :new
+    end
+  end
+  def edit
+    @project = Project.find(params[:id])
+  end
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      redirect_to project_path(@project)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    redirect_to projects_path
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:name, :link, :picture, :description, :skill_id)
+    end
+end
+
