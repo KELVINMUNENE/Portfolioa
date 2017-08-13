292f9a0f6c777a88ff939140f0595fe2392e6dd4
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-13 18:01:13 +0300

    created a column for name and rating

--- db/migrate/20170813145829_create_skills.rb
+++ db/migrate/20170813145829_create_skills.rb
@@ -1,6 +1,12 @@
 class CreateSkills < ActiveRecord::Migration[5.1]
   def change
     create_table :skills do |t|
+    t.column :name, :string
+    t.column :ability, :integer
+  
+
+     t.timestamps
+
     end
   end
 end
