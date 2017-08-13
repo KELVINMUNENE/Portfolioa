d3bd4cedae2624429350e77dfda1506920e4b3df
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-13 19:17:27 +0300

    changed the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root :to => 'main#index'
- resources :skills do
-	get "skills"
- end
+  resources :projects do
+    get "skill"
+  end
+  resources :skills do
+    get "project"
+  end
 end
-
-
