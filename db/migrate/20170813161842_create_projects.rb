ea04ec637c1a04818a148c6e320e67e631b0d051
KELVINMUNENE <karvinmitchar@yahoo.com>
2017-08-13 19:21:56 +0300

    created a column for projects

--- db/migrate/20170813161842_create_projects.rb
+++ db/migrate/20170813161842_create_projects.rb
@@ -1,6 +1,14 @@
 class CreateProjects < ActiveRecord::Migration[5.1]
   def change
     create_table :projects do |t|
+    	t.column :name, :string
+    	t.column :project_link, :varchar
+    	t.column :description, :string
+
+    	t.timestamps
     end
   end
 end
+
+
+
